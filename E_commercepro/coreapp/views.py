0740d9e8e26ba967d7cf12d9541a9d6c02bbcd3a
Bharathiraja2806 <bharathiraja9708@gmail.com>
2025-01-31 09:26:40 +0530

    product desc

--- E_commercepro/coreapp/views.py
+++ E_commercepro/coreapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from coreapp.form import *
 from django.contrib import messages
+from django.utils import timezone
 
 def index(request):
     products = Product.objects.all()
@@ -21,3 +22,44 @@ def add_product(request):
     else:
         form = ProductForm()
     return render(request, "coreapp/add_product.html", {"form": form})
+
+def product_desc(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, "coreapp/product_des.html", {"product": product})
+
+
+def add_to_cart(request, pk):
+
+    # get that particular product id=pk
+    product = Product.objects.get(pk = pk)
+
+    # create order item
+    order_item, created = OrderItem.objects.get_or_create(
+        product = product,
+        user = request.user,
+        ordered = False,
+    )
+
+     # Get Query set of Order Object of Particular User
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(product__pk=pk).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Added Quantity Item")
+            return redirect("product_desc", pk=pk)
+        else:
+            order.items.add(order_item)
+            messages.info(request, "Item added to Cart")
+            return redirect("product_desc", pk=pk)
+
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "Item added to Cart")
+        return redirect("product_desc", pk=pk)
+
+
+
